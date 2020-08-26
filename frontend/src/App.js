22793908111b509ae1e28ac4e98865c995a202bd
Eric Doppelt <ead45@duke.edu>
2020-08-26 15:05:37 -0400

    implemented championship page for 2001 with image, text, and link to highlights
    resolves #11

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import HomePage from './Home/HomePage';
 import Championship1991 from './Championships/Championship1991';
 import Championship1992 from './Championships/Championship1992';
-
+import Championship2001 from './Championships/Championship2001';
 
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
           <Route path="/" exact component={HomePage}/>
           <Route path="/championships/1991" component={Championship1991}/>
           <Route path='/championships/1992' component={Championship1992}/>
+          <Route path='/championships/2001' component={Championship2001}/>
         </div>
       </Router>
     );
---
+++ frontend/src/Championships/Championship2001.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {Typography} from '@material-ui/core';
+import ChampionshipImage from '../resources/Championship2001.jpg';
+import '../CommonStylings/CenteredImage.css';
+import '../CommonStylings/FullDiv.css';
+import ReturnButton from './ReturnButton';
+
+function Championship2001(props) {
+    return(
+        <div class='fullDiv'>
+            <Typography align='center' variant="h1"> Duke Championship: 2001</Typography>
+
+            <div class='centeredImage'>
+                <img alt='duke championship 1992' src={ChampionshipImage}></img>
+            </div>
+            
+            <Typography align='center' variant='h6'>
+                On April 6, 1992, Duke won its second national championship in Minneapolis, MN by beating the Arizona Wildcats. 
+                <br/>
+                Duke entered the NCAA Tournament as the #1 seed, led by Shane Battier who was the tournament's Most Valuable Player.
+                <br/>
+                Duke also set the record for the largest comeback in Final Four history, coming back from down 22 points to beat the Maryland Terrapins.
+                <br/>
+                <br/>
+                UNC lost to Penn State in the Round of 32 in 2001. They were the #2, so that's really bad.
+                <br/>
+                <br/>
+                You can watch highlights from the game <a href='https://www.youtube.com/watch?v=ctRN1ATtORY'>here</a>.
+            </Typography>
+
+            <ReturnButton/>
+        </div>
+    );
+}
+
+export default Championship2001;
