42179d8bff737188d5c1081c20330c577adc00a8
Eric Doppelt <ead45@duke.edu>
2020-08-26 14:04:50 -0400

    refactored common CSS into a central directory
    fixes #8

--- frontend/src/Championships/Championship1991.js
+++ frontend/src/Championships/Championship1991.js
@@ -5,7 +5,8 @@ import {Typography} from '@material-ui/core';
 function Championship1991(props) {
     return(
         <div>
-            <Typography variant="h1"> Made it!</Typography>
+            <Typography align='center' variant="h1"> Duke Championship: 1991</Typography>
+            
         </div>
     );
 }
--- frontend/src/Home/HomePage.js
+++ frontend/src/Home/HomePage.js
@@ -2,17 +2,18 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import {Button, Typography} from '@material-ui/core';
 import ChampionshipButton from './ChampionshipButton';
-import './HomePage.css';
+import '../CommonStylings/CenteredImage.css';
+import '../CommonStylings/FullDiv.css';
 
 import ChampionshipImage from '../resources/ChampionshipBanner.jpeg';
 
     
 function HomePage(props) {
     return(
-        <div>
+        <div class='fullDiv'>
             <Typography align='center' variant="h1"> Welcome to my CS408 Spike!</Typography>
             <Typography align='center' variant="h3"> Please choose a Duke NCAA National Champtionship.</Typography>
-            <div class='bannerImage'>
+            <div class='centeredImage'>
                 <img src={ChampionshipImage}></img>
             </div>
             <ChampionshipButton year='1991'/>
