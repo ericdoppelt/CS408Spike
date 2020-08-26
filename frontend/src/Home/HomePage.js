153caf4f1248a112dbc7bdbb66de7ed0baf860fc
Eric Doppelt <ead45@duke.edu>
2020-08-26 02:06:37 -0400

    created a home page using react-router-dom without links to other pages
    progress towards #5

---
+++ frontend/src/Home/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Route} from 'react-router-dom';
+import {Button, Typography} from '@material-ui/core';
+
+function HomePage(props) {
+    return(
+        <div>
+            <Typography variant="h1"> Welcome to my CS408 Spike!</Typography>
+        </div>
+    );
+}
+
+export default HomePage;
