42d65cb87e8e2d51335a2ad8eb4dfcc619373a43
Eric Doppelt <ead45@duke.edu>
2020-08-26 02:34:26 -0400

    added a single route to /1991 to have multiple pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 import HomePage from './Home/HomePage';
+import Championship1991 from './Championships/Championship1991';
+
 
 function App() {
   return (
       <BrowserRouter>
         <div className='App' style={{display: 'flex', width:'100%'}}>
           <Route path="/" exact component={HomePage}/>
+          <Route path="/1991" exact component={Championship1991}/>
         </div>
       </BrowserRouter>
     );
---
+++ frontend/src/Championships/Championship1991.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Route} from 'react-router-dom';
+import {Typography} from '@material-ui/core';
+
+function Championship1991(props) {
+    return(
+        <div>
+            <Typography variant="h1"> Made it!</Typography>
+        </div>
+    );
+}
+
+export default Championship1991;
