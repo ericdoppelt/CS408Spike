1dffe295649b9c02c2a31bd7ebd313aedabb85e6
Eric Doppelt <ead45@duke.edu>
2020-09-01 20:11:36 -0400

    imported the time flask api call into the frontend
    resolves #15

--- frontend/src/Home/HomePage.js
+++ frontend/src/Home/HomePage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {Typography} from '@material-ui/core';
 import ChampionshipButton from './ChampionshipButton';
 import ChampionshipImage from '../resources/ChampionshipBanner.jpeg';
@@ -8,10 +8,20 @@ import '../CommonStylings/FullDiv.css';
 
     
 function HomePage(props) {
+    const [currentTime, setCurrentTime] = useState(0);
+
+    useEffect(() => {
+        fetch('/time').then(res => res.json()).then(data => {
+          setCurrentTime(data.time);
+        });
+      }, []);
+
+
     return(
         <div class='fullDiv'>
             <Typography align='center' variant="h1"> Welcome to my CS408 Spike!</Typography>
             <Typography align='center' variant="h3"> Please choose a Duke NCAA National Champtionship.</Typography>
+            <Typography> {currentTime} </Typography>
             <div class='centeredImage'>
                 <img alt='duke championship banners' src={ChampionshipImage}></img>
             </div>
@@ -22,8 +32,15 @@ function HomePage(props) {
             <ChampionshipButton year='2015'/>
         </div>
     );
+
+
 }
 
-
+function getTime() {
+    let time;
+    fetch('/time').then(res => res.json()).then(data => (time = data));
+    console.log(time);
+    return time;
+}
 
 export default (HomePage);
